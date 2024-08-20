96c10ea1c86dcb642290ed9be5cb56b8e2cf3a57
Nicolas <nicolas.merget@gmail.com>
2024-08-21 00:20:48 +0200

    fix: issue with manifest icons

--- manifest.ts
+++ manifest.ts
@@ -12,13 +12,13 @@ const manifest: Partial<ManifestOptions> = {
   scope: "/",
   icons: [
     {
-      src: "/right/icons/manifest-icon-192.maskable.png",
+      src: "/sgh-schedule/icons/manifest-icon-192.maskable.png",
       sizes: "192x192",
       type: "image/png",
       purpose: "any maskable",
     },
     {
-      src: "/right/icons/manifest-icon-512.maskable.png",
+      src: "/sgh-schedule/icons/manifest-icon-512.maskable.png",
       sizes: "512x512",
       type: "image/png",
       purpose: "any maskable",
